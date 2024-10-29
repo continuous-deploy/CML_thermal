61c4c7e84173595758b6bb50c52d75456105a5ff
CML_thermal <cmldeployment@gmail.com>
2024-10-29 16:07:56 +0530

    projectarchitecture setup for comparison of model

---
+++ models/ann.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import model
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as pd
+
---
+++ train.py
@@ -0,0 +1,11 @@
+"""
+This file will trigger model retrainning after importingg all the models from model folder
+"""
+
+from models import *
+
+
+
+
+
+#Save into metrics
